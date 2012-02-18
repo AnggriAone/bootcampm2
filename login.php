05005e28804ee0abf7e3c02f1af3e8c5eb6de1ed
Fanani M. Ihsan <contact@fanani.net>
2012-02-18 11:20:01 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>login</title>
+        <link href="js/dojox/mobile/themes/android/android.css" rel="stylesheet"></link>
+        <script src="js/dojo/dojo.js" data-dojo-config="isDebug: true, parseOnLoad: true"></script>
+        
+        <script>
+            dojo.require("dojox.mobile.parser");
+            dojo.require("dojox.mobile");
+            dojo.requireIf(!dojo.isWebKit, "dojox.mobile.compat");
+
+        </script>
+    </head>
+    <body>
+        <h1>hello</h1>
+        <p>Selamt Belajar Dojo</p>
+    </body>
+</html>
