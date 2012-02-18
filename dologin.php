16c735e4cd6df63c4a0947f5427ec35fc6637143
Fanani M. Ihsan <contact@fanani.net>
2012-02-18 12:36:37 +0700

    validasi login

--- dologin.php
+++ dologin.php
@@ -1,21 +1,23 @@
 <?php
-    $user = $POST['username'];
-    $pass = $POST['password'];
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
     
     session_start();
     
-    if (strtoupper(trim($user) == 'ADMIN') && strtoupper(trim($pass) == 'ADMIN') ){
-        $__SESSION('user') = trim($user);
+    if ( strtoupper(trim($user)) == 'ADMIN' ){
+        $__SESSION['user'] = trim($user);
         header("Location: http://localhost/bootcampm2/mobile.php");
     }else{
         session_destroy();
-        $__SESSION('user') = '';
+        echo "destroy";
+        $__SESSION['user'] = '';
         header("Location: http://localhost/bootcampm2/login.php");
     }
     
     if ($_GET['a'] == 'logout' ){
         session_destroy();
-        $__SESSION('user') = '';
+        $__SESSION['user'] = '';
         header("Location: http://localhost/bootcampm2/");
+        echo "other";
     }
 ?>
